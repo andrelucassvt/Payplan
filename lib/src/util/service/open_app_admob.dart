54e141cb0b43f3a827da6bf5072a6dfd573700f5
André_Lucas <andrelucassvt99@gmail.com>
2024-12-17 13:17:20 -0300

    nova versap

--- lib/src/util/service/open_app_admob.dart
+++ lib/src/util/service/open_app_admob.dart
@@ -5,7 +5,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AppOpenAdManager {
   String adUnitId = Platform.isAndroid
       ? 'ca-app-pub-3652623512305285/5018170803'
-      : 'ca-app-pub-3940256099942544/5575463023';
+      : 'ca-app-pub-3652623512305285/1467067906';
 
   AppOpenAd? _appOpenAd;
 
